929bd10496bfda45d4b154c5faafc58bae2fd810
Wesllen <38706880+WesllenSF@users.noreply.github.com>
2025-06-07 19:12:25 -0300

    Configurado o modal e adicionando informações para serem exibidas

---
+++ App.tsx
@@ -0,0 +1,94 @@
+
+import {StyleSheet, Text, View, FlatList, Image} from 'react-native';
+import Produtos from './componentes/produto';
+
+  const produtos=[
+    { 
+      id:"1",
+      nome:"MAJIN VEGETA DRAGON BALL Z",
+      categoria:"Anime",
+      preco:"148,00",
+      descricao:"Vegeta é um personagem fictício da franquia Dragon Ball criado por Akira Toriyama",
+      marca:"BANPRESTO",
+    },
+    {
+      id:"2", 
+      nome:"SHEN LONG VERDE DB SUPER: SUPER HERO", 
+      categoria:"Anime", 
+      preco:"3.555,00", 
+      descricao:"Shenlong (神龙) é um dragão da mitologia chinesa, considerado um espírito e um deus das tempestades e da chuva", 
+      marca:"BANDAI"
+    },
+    {
+      id:"3", 
+      nome:"MESTRE KAME JACKEY CHUN DRAGON BALL Z", 
+      categoria:"Anime", 
+      preco:"888,00", 
+      descricao:"Mestre Kame, também conhecido no mangá como Muten Roshi é um personagem e protagonista de suporte da franquia Dragon Ball", 
+      marca:"BANDAI"
+    },
+    {
+      id:"4", 
+      nome:"GOHAN KID SSJ DRAGON BALL Z GX", 
+      categoria:"Anime", 
+      preco:"330,00", 
+      descricao:"Son Gohan, também conhecido apenas como Gohan, é um personagem fictício da franquia Dragon Ball criado por Akira Toriyama", 
+      marca:"BANPRESTO"
+    },
+    {
+      id:"5", 
+      nome:"SON GOKU KID NA CABEÇA DO SHEN LONG DRAGON BALL Z", 
+      categoria:"Anime", 
+      preco:"1.545,00", 
+      descricao:"Son Goku, cujo nome de nascimento é Kakarotto, é o protagonista da franquia Dragon Ball", 
+      marca:"BANPRESTO"
+    },
+    {
+      id:"6", 
+      nome:"SON GOKU ANGEL DRAGON BALL Z", 
+      categoria:"Anime", 
+      preco:"235,00", 
+      descricao:"Son Goku, cujo nome de nascimento é Kakarotto, é o protagonista da franquia Dragon Ball", 
+      marca:"BANPRESTO"
+    },
+  ]
+
+export default function App() {
+
+  return (
+    <View style={styles.container}>
+
+            <View>
+                      <Text style={styles.titulo}>Action Figures</Text>
+            </View>                
+                <FlatList
+                  data={produtos}
+                  renderItem={({item}) => 
+                  <Produtos 
+                  nome={item.nome} 
+                  categoria={item.categoria} 
+                  preco={item.preco} 
+                  descricao={item.descricao} 
+                  marca={item.marca}
+                  />}
+
+                  keyExtractor={item => item.id}
+                />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: '15%',
+    width: '90%',
+    marginLeft: '5%',
+    flex: 1,
+    backgroundColor: 'white',
+  },
+    titulo: {
+    fontSize: 35,
+    textAlign: 'center',
+    fontWeight:'bold',
+    },
+});
---
+++ componentes/produto.tsx
@@ -0,0 +1,129 @@
+import {Alert, Modal, StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
+import React, {useState} from 'react';
+interface propsProduto{
+    nome:string;
+    categoria:string;
+    preco:string;
+    descricao:string;
+    marca:string;
+}
+
+export default function Produtos({nome, categoria, preco, descricao, marca}:propsProduto){
+    const [modalVisible, setModalVisible] = useState(false);
+    return (
+      
+        <>
+
+            {/* Modal onde exibe os detalhes dos produtos */}
+                        <Modal
+                            animationType="slide"
+                            transparent={true}
+                            visible={modalVisible}
+                            onRequestClose={() => {
+                              Alert.alert('Modal ');
+                              setModalVisible(!modalVisible);
+                            }}>
+                            <View style={styles.centeredView}>
+                              <View style={styles.modalView}>
+
+                                <Text style={styles.nome}>Nome: {nome}</Text>
+                                <Text style={styles.categoria}>Categoria: {categoria} </Text>
+                                <Text style={styles.categoria}>Marca: {marca}</Text>
+                                <Text style={styles.categoria}>Descrição: {descricao} </Text>
+                                <Text style={styles.categoria}>Preço: {preco}</Text>                                
+
+                                <TouchableOpacity
+                                  style={[styles.button, styles.buttonClose]}
+                                  onPress={() => setModalVisible(!modalVisible)}>
+                                  <Text style={styles.botao}>Fechar</Text>
+                                </TouchableOpacity>
+                              </View>
+                            </View>
+                          </Modal>
+            {/* Modal onde exibe os detalhes dos produtos */}
+
+            {/* Botão onde exibe cada produto */}
+                <TouchableOpacity
+                    onPress={() => setModalVisible(true)}
+                >
+                    {/* Chama exibe os conteudos */}
+                      <View style={styles.cartao}>
+                          <Image />
+                          <Text style={styles.nome}>Nome: {nome} </Text>
+                          <Text style={styles.preco}>Categoria: {categoria}</Text>
+                          <Text style={styles.preco}>Preço: {preco}</Text>
+                      </View>
+                    {/* Chama exibe os conteudos */}
+                </TouchableOpacity>
+            {/* Botão onde exibe cada produto */}
+          
+        </>
+    );
+}
+
+
+const styles = StyleSheet.create({
+  cartao: {
+    marginTop: 15,
+    borderRadius: 10,
+    backgroundColor: 'aqua',
+    padding: '2%',
+    marginLeft: '5%',
+    width: '90%',
+ },
+  nome: {
+    fontSize: 25,
+    fontWeight:'bold',
+    marginBottom: 15,
+  },
+  preco: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    shadowColor: '#000',
+    shadowOffset: {
+    width: 0,
+    height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    width: '100%',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  categoria: {
+    textAlign: 'left',
+    marginBottom: 5,
+    fontSize: 18,
+  },
+  botao: {
+    textAlign: 'center',
+    fontSize: 15,
+    color: 'white',
+  },
+});
